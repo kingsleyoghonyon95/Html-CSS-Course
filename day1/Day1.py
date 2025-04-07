fa1e0cc64cf10aa421bdc9db01b79152b41b33bf
kingsleyoghonyon95 <kingsleyigho35@gmail.com>
2025-04-06 22:28:55 -0500

    initial commit

--- day1/Day1.py
+++ day1/Day1.py
@@ -1,5 +1,27 @@
-last_name = "Igho"
-print(last_name)
+# last_name = "Igho"
+# print(last_name)
 
-last_name = "John"
-print(last_name)
+# last_name = "John"
+# print(last_name)
+
+my_tuple = ('A', 2, 'B', 7, 'C', '%', 'A', 10, 'A', '&')
+print(my_tuple)
+
+first_index = my_tuple.index('A')
+print(first_index)
+
+second_index = my_tuple.index('A', first_index + 1)
+print(second_index)
+
+third_index = my_tuple.index('A', second_index + 1)
+print(third_index)
+
+new_tuple = (first_index, second_index, third_index)
+print(new_tuple)
+
+second_newtuple = (my_tuple[first_index], my_tuple[second_index], my_tuple[third_index])
+print(second_newtuple)
+
+print(f"Formatted output: {second_newtuple}")
+
+
